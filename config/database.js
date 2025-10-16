87a4215cea8aa5622dccfa5e4a131302e00d1266
kauemarini <kauemarini@gmail.com>
2025-10-16 05:10:20 -0300

    feat: Adiciona configuração de database explícita para produção

--- config/database.js
+++ config/database.js
@@ -1,35 +1,14 @@
-const path = require('path');
-
-module.exports = ({ env }) => {
-  if (env('NODE_ENV') === 'production') {
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          user: env('DATABASE_USERNAME', 'strapi'),
-          password: env('DATABASE_PASSWORD', 'strapi'),
-          ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
-        },
-        debug: false,
-      },
-    };
-  }
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(
-          __dirname,
-          '..',
-          env('DATABASE_FILENAME', '.tmp/data.db')
-        ),
-      },
-      useNullAsDefault: true,
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
     },
-  };
-};
-
+    debug: false,
+  },
+});
